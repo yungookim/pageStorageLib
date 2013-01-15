fe1da9517c69f5d3b441a073f421b9215403573a
Jaehwan Ryu <jaehwanryu@user129-196.wireless.utoronto.ca>
2013-01-15 18:12:51 -0500

    time added

--- PL1/create_random_file.c
+++ PL1/create_random_file.c
@@ -1,10 +1,13 @@
 #include <stdio.h> 
+#include <time.h>
 
 int main(int argc, char *argv[]) { 
 	if (argc != 4){
 		printf( "Must put <filename> <total bytes> <block size>\n" );
 		return 0;
 	}
+
+	clock_t t1, t2;
 
 	FILE *file; 
 	file = fopen(argv[1],"w+");
@@ -15,6 +18,7 @@ int main(int argc, char *argv[]) {
 
 	char buffer[block_size];
 	
+	t1 = clock();
 	while (written_size < total_byte) {
 		int written = 0;
 		while (written < block_size){
@@ -25,6 +29,10 @@ int main(int argc, char *argv[]) {
 		fflush(file);
 		written_size += block_size;
 	}
+	t2 = clock();
+	float diff = (((float)t2 - (float)t1) / 1000000.0F ) * 1000;
+    printf("%f\n",diff);
+    
 
 	fclose(file); /*done!*/ 
 	return 0; 

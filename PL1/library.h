0b2f205ee0190b204333128f534d5ea74f720ca1
Jaehwan Ryu <rjh0121@hotmail.com>
2013-01-15 22:39:20 -0500

    get_histogram declared

--- PL1/library.h
+++ PL1/library.h
@@ -1,5 +1,13 @@
+#include <stdio.h>
 /**
  * populate a random array (which is already
  * allocated with enough memory to hold n bytes.
  */
 void random_array(char *array, long bytes);
+
+int get_histogram(
+	FILE *file_ptr,
+	long hist[],
+	int block_size,
+	long *milliseconds,
+	long *total_bytes_read);

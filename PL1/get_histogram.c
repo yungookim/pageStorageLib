6b981b815e34072d4083f45c48e01bc4143faaf4
Jaehwan Ryu <rjh0121@hotmail.com>
2013-01-18 01:25:10 -0500

    little change

---
+++ PL1/get_histogram.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "library.h"
+int main(int argc, char *argv[]) { 
+
+	//implement this
+	
+	return 0; 
+}
--- PL1/library.h
+++ PL1/library.h
@@ -5,6 +5,16 @@
  */
 void random_array(char *array, long bytes);
 
+/**
+ * file_ptr : the file pointer, ready to be read from.
+ * hist: an array to hold 26 long integers.  hist[0] is the
+ *       number of 'A', and hist[1] is the number of 'B', etc.
+ * block_size: the buffer size to be used.
+ * milliseconds: time it took to complete the file scan
+ * total_bytes_read: the amount data in bytes read
+ *
+ * returns: -1 if there is an error.
+ */
 int get_histogram(
 	FILE *file_ptr,
 	long hist[],

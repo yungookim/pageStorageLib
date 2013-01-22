3866276e91626740cdf8928388b72db616bd3f73
Yungoo Kim <yungookim@gmail.com>
2013-01-22 18:39:11 -0500

    bug fixes and stats

--- PL1/get_histogram.c
+++ PL1/get_histogram.c
@@ -2,33 +2,31 @@
 #include "library.h"
 int main(int argc, char *argv[]) { 
 
-	/*
-		int i;
-		char c = 'B';
-		i = (int)c;
-		printf("%d\n", i);
-	*/
 	long hist[26];
 	long milliseconds;
 	long filelen;
 
-	FILE *file_ptr = fopen("1.test", "r");
+	FILE *file_ptr = fopen(argv[1], "r");
+	int block_size = atoi(argv[2]);
 	/**
 	 * Compute the histogram using 2K buffers
 	 */
 	int ret = get_histogram( file_ptr, 
 	                         hist, 
-	                         1,
+	                         block_size,
 	                         &milliseconds,
 	                         &filelen);
 
-	printf("1\n");
 	printf("Computed the histogram in %lu ms.\n", milliseconds);
 	int i;
 	for(i=0; i < 26; i++) {
-    	printf("%c : %lu\n", 'A' + i, hist[i]);
+    		printf("%c : %lu\n", 'A' + i, hist[i]);
 	}
 	printf("Data rate: %f Bps\n", (double)filelen/milliseconds * 1000);
 		
+	printf("BLOCK SIZE %d bytes\n", block_size);
+	printf("TOTAL BYTES %ld bytes\n", filelen);
+	printf("TIME %lu\n", milliseconds);
+
 	return 0; 
 }

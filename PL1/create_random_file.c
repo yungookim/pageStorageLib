fcb98da9276ff1e6b7a971d24a911c75fa4e140e
Yungoo Kim <yungookim@gmail.com>
2013-01-22 17:37:07 -0500

    merging

--- PL1/create_random_file.c
+++ PL1/create_random_file.c
@@ -2,28 +2,33 @@
 #include "library.h"
 #include <time.h>
 #include <fcntl.h>
+#include <sys/timeb.h>
 
 int main(int argc, char *argv[]) { 
 	if (argc != 4){
 		printf( "Must put <filename> <total bytes> <block size>\n" );
 		return 0;
 	}
-	printf("1\n");
 	clock_t t1, t2;
 
 	int total_byte = atoi((argv[2]));
 	int block_size =  atoi((argv[3]));
 	int written_size = 0;
 
-	char buffer[total_byte];
+	char buffer[block_size];
 	// Let it flush right to the disk
 	int openFlags = O_CREAT | O_WRONLY | O_SYNC;
 
-	printf("2\n");
 	//Open it and give owner permission!
 	int fd = open(argv[1], openFlags, S_IRUSR | S_IWUSR);
 	int byteWritten;
 	t1 = clock();
+	
+struct timeb _t;
+ftime(&_t);
+long init = _t.time * 1000 + _t.millitm;
+printf("- %lu", init);
+
 	for (byteWritten = 0; byteWritten < total_byte; byteWritten += block_size){
 		random_array(buffer, block_size);
 		write(fd, buffer, block_size);
@@ -31,10 +36,16 @@ int main(int argc, char *argv[]) {
 	t2 = clock();
 	//Close the fd
 	close(fd);
-
-	printf("3\n");
+/*
 	float diff = (((float)t2 - (float)t1) / 1000000.0F ) * 1000;
 	printf("%f\n",diff);
+*/
+
+struct timeb _t1;
+ftime(&_t1);
+long done = _t1.time * 1000 + _t1.millitm;
+printf("+ %lu", done);
+printf(" = %lu\n", done-init);
 
 	return 0;	
 /*	FILE *file; 

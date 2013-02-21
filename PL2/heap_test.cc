abb1aaa0fef1690a35f616c5f1726bb505c16352
Yungoo Kim <yungookim@gmail.com>
2013-02-20 19:43:53 -0500

    indentation fixes

--- PL2/heap_test.cc
+++ PL2/heap_test.cc
@@ -52,35 +52,35 @@ int main( int argc, const char* argv[] )
 	
 	data.close();
 
-        Page *page1 = (Page*)malloc(sizeof(Page));
-        page1->page_size = page_size;
-        page1->slot_size = 1000;
-        page1->data = (char*)malloc(sizeof(char)*page_size);
-        read_page(hf, 1, page1);
+  Page *page1 = (Page*)malloc(sizeof(Page));
+  page1->page_size = page_size;
+  page1->slot_size = 1000;
+  page1->data = (char*)malloc(sizeof(char)*page_size);
+  read_page(hf, 1, page1);
 
-        int SLOT_SIZE = 1000;
-        int NUMB_ATTRIBUTE = 100;
-        
-        //Initialize a record(creating a schema)
-        Record record;
-        for(int i = 0; i < NUMB_ATTRIBUTE; i++){
-            V content = "          "; 
-            record.push_back(content);
-        }
+  int SLOT_SIZE = 1000;
+  int NUMB_ATTRIBUTE = 100;
+  
+  //Initialize a record(creating a schema)
+  Record record;
+  for(int i = 0; i < NUMB_ATTRIBUTE; i++){
+      V content = "          "; 
+      record.push_back(content);
+  }
 
-        for (int slot = 0; slot < fixed_len_page_capacity(page1); slot++){
-            //Read the page into records
-            read_fixed_len_page(page1, slot, &record);
+  for (int slot = 0; slot < fixed_len_page_capacity(page1); slot++){
+    //Read the page into records
+    read_fixed_len_page(page1, slot, &record);
 
-            //print out this record in csv form
-            for(int i = 0; i < record.size(); i++){
-                if(i == record.size() - 1){
-                    printf("%s\n", record.at(i));
-                } else {
-                    printf("%s,", record.at(i));
-                }
-            }
-        }
+    //print out this record in csv form
+    for(int i = 0; i < record.size(); i++){
+      if(i == record.size() - 1){
+          printf("%s\n", record.at(i));
+      } else {
+          printf("%s,", record.at(i));
+      }
+    }
+  }
 
 	fclose(f);
 	return 0;
